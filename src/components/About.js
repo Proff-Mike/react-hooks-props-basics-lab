453ccb21234d4773731b941296f26b05945b20c6
Michael Okoe <michael.okoejr@gmail.com>
2022-07-21 12:02:21 +0000

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
+function About(props) {
+  const infoBio = (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <p>{props.bio}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" /></p>
       {/* add your <Links /> component here */}
+      <Links github = {props.links.github} linkedin = {props.links.linkedin} />
     </div>
   );
+  
+  const noBio = (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {/* add your <Links /> component here */}
+      <Links github = {props.links.github} Linkedin = {props.links.Linkedin} />
+    </div>
+  )
+
+  const showBio = props.bio
+    if (showBio) {
+      return infoBio
+    }
+    else {
+      return noBio
+    }   
 }
 
 export default About;
